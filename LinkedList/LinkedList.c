dc8284004075e78597df40c29eb811a10efccdb1
Ayush Kumar Verma <ayushkumarverma97@outlook.com>
2024-12-19 18:14:10 +0530

    [Linked List] Add function to insert node before a given node

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct Node
 {
     int data;
     struct Node *next;
 }Node;
 
+Node * insertBeginning(int val, Node *head);
+Node * insertBeforeNode(int val, int node, Node * head);
 
 Node * insertBeginning(int val, Node *head)
 {
     /* Create a new temp node with value to be inserted */
-    Node * temp = (Node *)malloc(sizeof(Node *));
+    Node * temp = (Node *)malloc(sizeof(Node));
     temp->data = val;
     temp->next = NULL;
 
@@ -27,6 +30,28 @@ Node * insertBeginning(int val, Node *head)
     return head;
 }
 
+
+Node * insertBeforeNode(int val, int node, Node * head)
+{
+    /* Create a new temp node with value to be inserted */
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->next = NULL;
+
+    Node *prev = head;
+    Node *curr = prev->next;
+
+    while(curr->data != node && curr != NULL)
+    {
+        curr = curr->next;
+        prev = prev->next;
+    }
+
+    prev->next = newNode;
+    newNode->next = curr;
+
+    return head;
+}
 
 void displayLinkedList(Node *head)
 {
@@ -49,6 +74,10 @@ int main()
     head = insertBeginning(2, head);
     head = insertBeginning(3, head);
     head = insertBeginning(4, head);
+
+    displayLinkedList(head);
+
+    head = insertBeforeNode(5, 3, head);
 
     displayLinkedList(head);
 
